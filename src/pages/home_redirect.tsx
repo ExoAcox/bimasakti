461063b6bb9b7b45a618d56553e4c5d49e79ebb1
Anas <anasarifin1995@gmail.com>
2026-09-13 15:01:28 +0700

    cicd: fix build

---
+++ src/pages/home_redirect.tsx
@@ -0,0 +1,11 @@
+import { redirect } from "react-router";
+
+export const clientLoader = () => {
+    return redirect("/");
+};
+
+const Redirect = () => {
+    return null
+}
+
+export default Redirect
--- src/routes.ts
+++ src/routes.ts
@@ -7,8 +7,8 @@ import {
 
 export default [
     layout("./components/Canvas.tsx", [
-        index("./pages/milky_way.tsx", { id: "home" }),
-        route("milky_way", "./pages/milky_way.tsx"),
+        index("./pages/milky_way.tsx"),
+        route("milky_way", "./pages/home_redirect.tsx"),
         route("solar_system", "./pages/solar_system.tsx"),
         route("alpha_centauri", "./pages/alpha_centauri.tsx"),
         route("trappist-1", "./pages/trappist-1.tsx"),
